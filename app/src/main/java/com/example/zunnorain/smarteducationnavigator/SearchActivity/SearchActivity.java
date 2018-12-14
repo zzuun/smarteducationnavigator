cc5a5ed71dff80b34036f6ece8eab8e270f04aea
Zunnorain Zaheer <zunnorainzaheer360@gmail.com>
2018-12-14 18:08:22 +0500

    search second commit

---
+++ app/src/main/java/com/example/zunnorain/smarteducationnavigator/SearchActivity/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.example.zunnorain.smarteducationnavigator.SearchActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.zunnorain.smarteducationnavigator.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
